e97b82e2ada2f6ff7fc367ea3ecf00ece32144a6
ItzEricAn <87569634+ItzEricAn@users.noreply.github.com>
2022-12-04 21:11:36 +0800

    AOC2015D1P2

---
+++ Day1Part2.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class Day1Part2 {
+    public static void main(String[] args) {
+        int count = 0;
+        Scanner input = new Scanner(System.in);
+        String c = input.nextLine();
+
+        for(int i = 0; i < c.length(); i++){
+            if(c.substring(i, i+1).equals("(")){
+                count++;
+            }else{
+                count--;
+                if(count == -1){
+                    count = i+1;
+                    break;
+                }
+            }
+        }
+        System.out.println(count);
+    }
+}
